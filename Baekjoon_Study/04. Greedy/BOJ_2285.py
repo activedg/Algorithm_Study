9f6305f324d3eb2babb71c28f72e994caa84f1a9
active_dg <nerw173@gmail.com>
2022-07-17 16:08:23 +0900

    [ADD] 05. Dynamic Programming 1

--- Baekjoon_Study/04. Greedy/BOJ_2285.py
+++ Baekjoon_Study/04. Greedy/BOJ_2285.py
@@ -1,13 +1,15 @@
 import sys
 inp = sys.stdin.readline
-village = {}
+village = []
+people = 0
 for _ in range(int(inp())):
     a, b = map(int, inp().split())
-    village[a] = b
-min_dist = [sys.maxsize, 0]
-keys = sorted(village.keys())
-for k in keys:
-    temp = sum(abs(i-k)*village[i] for i in keys if i != k)
-    if temp < min_dist[0]:
-        min_dist = [temp, k]
-print(min_dist[1])
+    village.append([a, b])
+    people += b
+village.sort(key=lambda x:x[0])
+cnt = 0
+for i, p in village:
+    cnt += p
+    if cnt >= people/2:
+        print(i)
+        break
