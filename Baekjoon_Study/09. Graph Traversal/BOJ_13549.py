fdb8ac191d1efe5a33eb217d7a8e134d2e34d62c
active_dg <nerw173@gmail.com>
2022-08-30 13:34:56 +0900

    [ADD] BOJ_12851

---
+++ Baekjoon_Study/09. Graph Traversal/BOJ_13549.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+inp = lambda : map(int, sys.stdin.readline().split())
+N, K = inp()
+max_size = 100000
+visit = [-1] * (max_size + 1)
+visit[N] = 0
+q = deque([N])
+while q:
+    t = q.popleft()
+    if t == K: break
+    for i in (t*2, t+1, t-1):
+        if 0 <= i <= max_size and visit[i] == -1:
+            if i == t*2:
+                visit[i] = visit[t]
+            else: visit[i] = visit[t] + 1
+            q.append(i)
+print(visit[K])
