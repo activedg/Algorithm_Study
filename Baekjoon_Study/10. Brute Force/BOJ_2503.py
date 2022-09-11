c79b347d3ca7aeaee8e1d33c759ef59c1665cc02
active_dg <nerw173@gmail.com>
2022-09-12 00:30:16 +0900

    [UPD] BOJ_2503 오류 수정

--- Baekjoon_Study/10. Brute Force/BOJ_2503.py
+++ Baekjoon_Study/10. Brute Force/BOJ_2503.py
@@ -5,17 +5,17 @@ N = int(inp())
 game = []
 for _ in range(N):
     game.append(list(map(int, inp().split())))
-## 111 부터 999까지 탐색하면서 맞는지 확인?
+## 111 부터 999까지 탐색하면서 맞는지 확인
+## itertools의 permutations를 사용하면 tuple로 만들어짐
 num = list(permutations([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 res = 0
 for i in range(len(num)):
     checker = True
     for n, s, b in game:
         n_digit = [n // 100, (n // 10) % 10, n % 10]
-        i_digit = [i // 100, (i // 10) % 10, i % 10]
         s_temp, b_temp = 0, 0
-        for n1, i1 in zip(n_digit, i_digit):
-            if i1 in n_digit:
+        for n1, i1 in zip(n_digit, num[i]):
+            if n1 in num[i]:
                 if n1 == i1:
                     s_temp += 1
                 else:
