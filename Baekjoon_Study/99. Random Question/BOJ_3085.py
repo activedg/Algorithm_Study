bc2d14b81b9283a0eb846fadd4320a75dbcecfc4
이동건 <nerw173@gmail.com>
2023-02-27 22:47:26 +0900

    [ADD] 사탕게임_3085

--- Baekjoon_Study/99. Random Question/BOJ_3085.py
+++ Baekjoon_Study/99. Random Question/BOJ_3085.py
@@ -1,6 +1,39 @@
-import sys, bisect
-inp = lambda : sys.stdin.readline()
-a = [5, 17, 6]
-b = [3, 9 , 13]
-for t in a:
-    print(bisect.bisect_left(b, t))
+import sys
+inp = lambda : sys.stdin.readline().rstrip()
+N = int(inp())
+candy = [list(inp()) for _ in range(N)]
+res = 1
+# 사탕 색이 다른 인접한 두 칸 고르기 -> 서로 교환 -> 가장 긴 연속 부분(행 또는 열) 고르고 먹기
+def candy_count():
+    global res
+    for i in range(N):
+        # 각 행 먼저 판별
+        cnt = 1
+        for j in range(1, N):
+            if candy[i][j-1] == candy[i][j]:
+                cnt += 1
+                res = max(res, cnt)
+            else:
+                cnt = 1
+    for j in range(N):
+        # 각 열 판별
+        cnt = 1
+        for i in range(1, N):
+            if candy[i][j] == candy[i-1][j]:
+                cnt += 1
+                res = max(res, cnt)
+            else:
+                cnt = 1
+candy_count()
+for i in range(N):
+    for j in range(N):
+        # 우측 으로만 교환
+        if j + 1 < N and candy[i][j] != candy[i][j+1]:
+            candy[i][j], candy[i][j + 1] = candy[i][j + 1], candy[i][j]
+            candy_count()
+            candy[i][j], candy[i][j + 1] = candy[i][j + 1], candy[i][j]
+        if i + 1 < N and candy[i][j] != candy[i+1][j]:
+            candy[i][j], candy[i + 1][j] = candy[i + 1][j], candy[i][j]
+            candy_count()
+            candy[i][j], candy[i + 1][j] = candy[i + 1][j], candy[i][j]
+print(res)
